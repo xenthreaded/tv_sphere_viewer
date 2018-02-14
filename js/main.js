8ef3f8135302478779d21aa25cf9235141608f05
Xenthreaded <xenthreaded@protonmail.com>
2018-02-14 21:05:12 +0100

    Reorganized code by creating Univers class

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+//TODO : clean code (let AL = this.AL !!!)
+
 class Etoile {
-    constructor(x, y, z, r, color) {
+    constructor(name, x, y, z, r, color) {
+        this.name = name;
         this.x = parseFloat(x);
         this.y = parseFloat(y);
         this.z = parseFloat(z);
@@ -28,78 +31,102 @@ class Etoile {
     }
 }
 
-window.onload = function() {
-    var WIDTH = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth,
-        HEIGHT = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-    var AL = WIDTH/300; //grosse approximation
+class Univers {
+    constructor(idCanvas, etoiles) {
+        this.WIDTH = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+        this.AL = this.WIDTH/300; //grosse approximation
+        let AL = this.AL; //degueulasse
 
-    var canvas = document.getElementById('map');
-    if(!canvas) {
-        alert("Impossible de récupérer le canvas");
-        return;
-    }
-
-    canvas.setAttribute("width", 400*AL);
-    canvas.setAttribute("height", 250*AL); 
-
-    var center_h = canvas.width/2;
-    var center_v = canvas.height/2;
-
-    var fantir_x = center_h;// 176*AL,
-        fantir_y = center_v;//100*AL,
-        fantir_z = 0;
-
-    var context = canvas.getContext('2d');
-    if(!context) {
-        alert("Impossible de récupérer le context du canvas");
-        return;
-    }
-    context.fillStyle = "#888";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    context.strokeStyle = "#fff";
-    context.beginPath()
-        context.moveTo(fantir_x, 0);
-        context.lineTo(fantir_x, canvas.height);
-        context.moveTo(0, fantir_y);
-        context.lineTo(canvas.width, fantir_y);
-        context.stroke();
-    context.closePath();
-
-    context.beginPath();
-        context.arc(fantir_x, fantir_y, 25*AL, 0, Math.PI*2);
-        context.arc(fantir_x, fantir_y, 100*AL, 0, Math.PI*2);
-        context.stroke();
-    context.closePath();
-    
-    let systemes = new Array();
-    //rayon du symbole != taille de l'étoile ; juste par souci de responsivité
-    let fantir = new Etoile(fantir_x, fantir_y, fantir_z, 1*AL, "#ff0"); 
-    fantir.draw(context);
-
-    let sol = new Etoile(fantir_x+15*AL, fantir_y-46.7*AL, fantir_z+35*AL, 1*AL, "#f00");
-    systemes.push(sol);
-    let alt = new Etoile(fantir_x+41.7*AL, fantir_y-21.3*AL, fantir_z-1*AL, 1*AL, "#f00");
-    systemes.push(alt);
-    let cchzlatzsstrill = new Etoile(fantir_x-23.4*AL, fantir_y+16.7*AL, fantir_z-2*AL, 1*AL, "#f50");
-    systemes.push(cchzlatzsstrill);
-    let avadi_arag = new Etoile(fantir_x-30*AL, fantir_y+25*AL, fantir_z-30*AL, 1*AL, "#00f");
-    systemes.push(avadi_arag);
-    let presidium = new Etoile(fantir_x-61*AL, fantir_y+40*AL, fantir_z-60*AL, 1*AL, "#000");
-    systemes.push(presidium);
-
-    for (let i = 0; i < systemes.length; i++) {
-        systemes[i].draw(context);
-    }
-
-    document.addEventListener('keyup', (event) => {
-        const keyName = event.key;
-
-        if (keyName === 'l') {
-            for (let i = 0; i < systemes.length; i++) {
-                systemes[i].rotate2D(10, fantir);
-                systemes[i].draw(context);
-            }
+        this.canvas = document.getElementById(idCanvas);
+        if(!this.canvas) {
+            alert("Can't get canvas");
+            return;
         }
-    }, false)
+
+        this.canvas.setAttribute("width", 400*AL);
+        this.canvas.setAttribute("height", 250*AL); 
+
+        this.center_h = this.canvas.width/2;
+        this.center_v = this.canvas.height/2;
+
+        this.fantir = new Etoile("fantir", this.center_h, this.center_v, 0, 1*AL, "#ff0"); 
+
+        //{nom, x, y, z, taille, couleur}
+        this.etoiles = new Array();
+        for (let i = 0; i < etoiles.length; i++) {
+            let current = etoiles[i];
+            this.etoiles.push(
+                new Etoile(
+                current.nom,
+                this.fantir.x + current.x*AL, 
+                this.fantir.y + current.y*AL, 
+                this.fantir.z + current.z*AL,
+                current.taille*AL,
+                current.couleur
+            ));
+        }
+
+        this.context = this.canvas.getContext('2d');
+        if(!this.context) {
+            alert("Can't get canvas' context");
+            return;
+        }
+    }
+
+    draw() {
+        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+        this.context.fillStyle = "#888";
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        //Axes
+        this.context.strokeStyle = "#fff";
+        this.context.beginPath()
+            this.context.moveTo(this.fantir.x, 0);
+            this.context.lineTo(this.fantir.x, this.canvas.height);
+            this.context.moveTo(0, this.fantir.y);
+            this.context.lineTo(this.canvas.width, this.fantir.y);
+            this.context.stroke();
+        this.context.closePath();
+
+        //Perimetres
+        this.context.beginPath();
+            this.context.arc(this.fantir.x, this.fantir.y, 25*this.AL, 0, Math.PI*2);
+            this.context.arc(this.fantir.x, this.fantir.y, 100*this.AL, 0, Math.PI*2);
+            this.context.stroke();
+        this.context.closePath();
+
+        this.fantir.draw(this.context);
+
+        for (let i = 0; i < this.etoiles.length; i++) {
+            this.etoiles[i].draw(this.context);
+        }
+
+    }
+
+    rotate2D(angle) {
+        for (let i = 0; i < this.etoiles.length; i++) {
+            this.etoiles[i].rotate2D(angle, this.fantir);
+        }
+    }
+}
+
+window.onload = function() {
+
+    let etoiles = [
+        {nom: "sol", x: 15, y: -46.7, z: 35, taille: 1, couleur: "#f00"},
+        {nom: "alt", x: 41.7, y: -21.3, z: -1, taille: 1, couleur: "#f00"},
+        {nom: "avadi_arag", x: -30, y: 25, z: -30, taille: 1, couleur: "#00f"},
+        {nom: "presidium", x: -61, y: 40, z: -60, taille: 1, couleur: "#000"},
+        {nom: "cchzlatzsstrill", x: -23.4, y: 16.7, z: -2, taille: 1, couleur: "#f50"}
+    ];
+
+    let sphere = new Univers("map", etoiles);
+
+    /*
+    let interval = setInterval(animate, 1000/60);
+    function animate() {
+        sphere.rotate2D(2);
+        sphere.draw();
+    }
+    */
 }
